75a6841c0a5b07092b42b973757914fd9ebf6f5c
Omri Spector <omri.spector@gmail.com>
2019-03-24 17:12:36 +0200

    bussiness logic

--- calculator.py
+++ calculator.py
@@ -17,4 +17,7 @@ choice = input("Enter choice(1/2/3/4):")
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-print("Invalid input ",choice)
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+else:
+   print("Invalid input ",choice)
