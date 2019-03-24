289a7a85f8f013d8eb0dbc51ca8434ebe16db115
Omri Spector <omri.spector@gmail.com>
2019-03-24 17:14:47 +0200

    placeholders for functions

--- calculator.py
+++ calculator.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 # Program make a simple calculator that can add, subtract, multiply and divide using functions
 
+# This function adds two numbers 
+def add(x, y):
+   #dummy implementation
+   return 1920
+
+# This function subtracts two numbers 
+def subtract(x, y):
+   #
+   return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+   #dummy implementation
+   return 1920
+
+# This function divides two numbers
+def divide(x, y):
+   return x / y
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
