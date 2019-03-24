188281e897c2dc354f296fd6580dbc626a6684b5
Omri Spector <omri.spector@gmail.com>
2019-03-24 17:24:04 +0200

    add mult and div

--- calculator.py
+++ calculator.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 # Program make a simple calculator that can add, subtract, multiply and divide using functions
 
+# This function multiplies two numbers
+def multiply(x, y):
+   return x * y
+
+# This function divides two numbers
+def divide(x, y):
+   return x / y
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
