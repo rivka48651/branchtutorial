26f729d36e094fdae6b5730ffc87ccfbee2fb673
Omri Spector <omri.spector@gmail.com>
2019-03-24 17:08:54 +0200

    initial commit

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Program make a simple calculator that can add, subtract, multiply and divide using functions
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+# Take input from the user 
+choice = input("Enter choice(1/2/3/4):")
+
+print("Invalid input ",choice)
